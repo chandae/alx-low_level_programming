7876da31d901640d3e01aaf8a0e065482de4904a
chandae <emmanuelvchanda1@gmail.com>
2022-03-15 20:18:24 +0200

    Adding files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*add - adds two numbers
+*@n: number to be added
+*@p: number to be added
+*Description - sum of two numbers
+*Return: sum.
+*/
+int add(int n, int p)
+{
+int sum = n + p;
+return (sum);
+}
