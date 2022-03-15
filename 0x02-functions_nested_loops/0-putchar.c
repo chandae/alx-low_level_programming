460c53951b7150c3393b0b5b95bca4bce3451222
chandae <emmanuelvchanda1@gmail.com>
2022-03-15 18:20:49 +0200

    Adding files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * main - program entry point
- * description - print the word alphabet
- * Return:0 Always (Success)
+ *main - program entry point
+ *Description - print the word alphabet
+ *Return:0 Always (Success)
  */
-
 int main(void)
 {
-	char i;
-	char p[9] = "_putchar";
+	int i;
+	char text[9] = "_putchar";
 
 	for (i = 0; i < 9; i++)
 	{
-		_putchar(p[i]);
+		_putchar(text[i]);
 	}
-	_putchar("\n");
+	_putchar('\n');
 
 	return (0);
 }
