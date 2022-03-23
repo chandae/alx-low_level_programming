1fec4ebbdc6854679da5c1429589b389b8e0031d
chandae <emmanuelvchanda1@gmail.com>
2022-03-23 22:21:42 +0200

    Adding file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,9 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int byteCount;
 
 	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+		{
 		dest[byteCount] = src[byteCount];
 		for (; byteCount < n; byteCount++)
+			{
 			dest[byteCount] = '\0';
-
+			}
+		}
 	return (dest);
 }
