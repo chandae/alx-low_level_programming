8b60292a8dec48adad747253ab34073f7185e20e
chandae <emmanuelvchanda1@gmail.com>
2022-04-12 22:01:03 +0200

    Adding project file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog -  initialises a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+}
