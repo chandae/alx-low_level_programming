e89a7577625dc86ae5646529e6d75fbc8b9f5791
chandae <emmanuelvchanda1@gmail.com>
2022-04-12 22:11:15 +0200

    Adding another project file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog -  prints the struct dog
+ * @d: pointer to dog of type struct dog
+ *
+ * Return: Always (0)
+ */
+
+void print_dog(struct * d)
+{
+if (d != NULL)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s\n",
+	d->name != NULL ? d->name : "(nil)",
+	d->age,
+	d->owner != NULL ? d->owner : "(nil)");
+}
+}
