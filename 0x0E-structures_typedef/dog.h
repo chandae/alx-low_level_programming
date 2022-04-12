353cdac30cae2370f58def6b4b0a99135fbaba28
chandae <emmanuelvchanda1@gmail.com>
2022-04-12 21:48:29 +0200

    Adding first project files including readme

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
