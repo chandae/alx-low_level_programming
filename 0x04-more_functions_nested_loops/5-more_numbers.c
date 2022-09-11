55f33a1b686863283277cadbd57246d4ab443fe9
Chandae <emmanuelvchanda1@gmail.com>
2022-09-11 06:32:23 +0200

    New file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* more_numbers - prints numbers from 0 t0 14 10 times
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar('0' + j);
+		}
+		_putchar('\n');
+	}
+}
