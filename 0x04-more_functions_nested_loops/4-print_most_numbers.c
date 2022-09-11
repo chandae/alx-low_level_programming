9ac3f223279d1410777db29db905f15353550498
Chandae <emmanuelvchanda1@gmail.com>
2022-09-11 06:19:53 +0200

    Update: quick code fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,11 @@ void print_most_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		if ((i != '2') && (i != '4'))
+		if ((i == 2) && (i == 4))
 		{
-			_putchar('0' + i);
-		{
+			continue;
+		}
+		_putchar('0' + i);
 	}
 	_putchar('\n');
 }
