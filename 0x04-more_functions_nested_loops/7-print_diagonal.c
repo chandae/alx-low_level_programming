1562edb452c722d906be990edfa4db6555a22b7f
Chandae <emmanuelvchanda1@gmail.com>
2022-09-11 07:06:17 +0200

    Commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_diagonal - print diagonal line on terminal
+* @n: length of line
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			/* print spaces first */
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
